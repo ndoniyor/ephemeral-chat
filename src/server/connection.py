f7cc65a7dea49392b41e998056ea95489c51bfc7
ndoniyor <ndoniyor123@gmail.com>
2023-12-14 18:58:40 -0500

    Refactor user management in Connection class

--- src/server/connection.py
+++ src/server/connection.py
@@ -1,38 +1,28 @@
 import server.proto.chat_pb2 as chat
 import server.proto.chat_pb2_grpc as rpc
 
-from server.user import User
-
-
 class Connection:
     def __init__(self):
-        self.user1 = None
-        self.user2 = None
+        self.users = ()
         self.chats = []
 
     @property
     def is_connected(self) -> bool:
-        return self.user1 and self.user2
+        return len(self.users) == 2
 
     def add_user(self, user: chat.ChatUser) -> bool:
-        try:
-            if not self.user1:
-                self.user1 = user
-            elif not self.user2:
-                self.user2 = user
+        if len(self.users) < 2:
+            self.users.add(user)
             return True
-        except Exception:
+        else:
             return False
 
     def add_to_chat(self, chat):
         self.chats.append(chat)
-    
+
     def remove_user(self, user: chat.ChatUser) -> bool:
-        try:
-            if self.user1 == user:
-                self.user1 = None
-            elif self.user2 == user:
-                self.user2 = None
-            return True
-        except Exception:
-            return False
+        for existing_user in self.users:
+            if existing_user.username == user.username:
+                self.users.remove(user)
+                return True
+        return False
