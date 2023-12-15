33c9391ab53c1f1384b725d2b079ef2734fa74b9
ndoniyor <ndoniyor123@gmail.com>
2023-12-15 17:30:15 -0500

    Refactor server.py to use ChatRoomInfo instead of ChatUserConnected

--- src/server/server.py
+++ src/server/server.py
@@ -3,7 +3,7 @@ import logging
 import server.proto.chat_pb2 as chat
 from server.proto.chat_pb2 import (
     ChatUser,
-    ChatUserConnected,
+    ChatRoomInfo,
     Empty,
     Message,
 )
@@ -13,28 +13,35 @@ from errors.errors import (
 )
 from connections.memory_manager import MemoryConnectionManager
 
-logging.basicConfig()
+CHAT_LIMIT_DEFAULT = 3
 
 
 class Server(ChatServicer):
     def __init__(self):
         self.connections = MemoryConnectionManager()
 
-    def Connect(self, request: ChatUser, context) -> ChatUserConnected:
+    def Connect(self, request: ChatUser, context) -> ChatRoomInfo:
         user = request
-        id = self.connections.get_or_create_available_connection(user)
+        id = self.connections.get_or_create_available_connection_id(user)
         status = id is not None
-        return ChatUserConnected(isConnected=status, conversationID=id)
+        return ChatRoomInfo(
+            isConnected=status, conversationID=id, userLimit=CHAT_LIMIT_DEFAULT
+        )
 
-    def Disconnect(self, request: ChatUser, context) -> ChatUserConnected:
-        status = self.connection.remove_user(request)
-        return ChatUserConnected(isConnected=not status)
+    def Disconnect(self, request: ChatUser, context) -> ChatRoomInfo:
+        user_id = request.username
+        conversation_id = request.conversationID
+        connection = self.connections.get_connection_by_conversation_id(conversation_id)
+        status = connection.remove_user(request)
+        if status:
+            logging.info(f"User {user_id} has successfully disconnected")
+        return ChatRoomInfo(isConnected=not status)
 
     def SendMessage(self, request: Message, context) -> Empty:
         logging.info(f"User {request.senderID} has sent a message: {request.message}")
         try:
-            connection = self.connections.get_connection_by_client_id(
-                request.senderID, request.conversationID
+            connection = self.connections.get_connection_by_conversation_id(
+                request.conversationID
             )
             connection.add_to_chat(request)
         except ConversationNotFoundError:
@@ -47,9 +54,8 @@ class Server(ChatServicer):
         last_seen_message_index = 0
 
         logging.info(f"User {client_id} has subscribed to messages")
-        connection = self.connections.get_connection_by_client_id(
-            client_id, conversation_id
-        )
+        connection = self.connections.get_connection_by_conversation_id(conversation_id)
+        # TODO make this run until connection closed
         while True:
             while len(connection.chats) > last_seen_message_index:
                 message = connection.chats[last_seen_message_index]
