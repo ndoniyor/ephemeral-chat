f568a770430812d7b4b2919a49df681a890c3f52
ndoniyor <ndoniyor123@gmail.com>
2023-12-14 23:15:37 -0500

    Refactor server.py to handle multiple connections

--- src/server/server.py
+++ src/server/server.py
@@ -1,36 +1,68 @@
 from concurrent import futures
 from collections import defaultdict
-from typing import Generator
+from typing import Generator, Tuple
 import logging
+from uuid import uuid4
 
 import grpc
 import server.proto.chat_pb2 as chat
-import server.proto.chat_pb2_grpc as rpc
+from server.proto.chat_pb2 import (
+    ChatUser,
+    ChatUserConnected,
+    Empty,
+    Message,
+)
+from server.proto.chat_pb2_grpc import ChatServicer
+from errors.errors import TooManyUsersError
 from server.connection import Connection
 
 
-class Server(rpc.ChatServicer):
+class Server(ChatServicer):
     def __init__(self):
-        self.connections = defaultdict(Connection)
+        self.connections = {}
 
-    def Connect(self, request: chat.ChatUser, context) -> chat.ChatUserConnected:
-        status = self.connection.add_user(request)
-        if status:
-            logging.info(f"User {request.username} has connected")
-        return chat.ChatUserConnected(isConnected=status)
+    # TODO: Directly add connection to available connections list
+    def _create_connection(self, user: ChatUser) -> str:
+        connection = Connection()
+        connection.add_user(user)
+        id = str(uuid4())
+        self.connections[id] = connection
+        return id
 
-    def Disconnect(self, request, context) -> chat.ChatUserConnected:
+    # TODO: Maintain list of available connections to reduce time complexity of search
+    def _get_or_create_available_connection(self, user: ChatUser) -> str | None:
+        for id, connection in self.connections.items():
+            if len(self.connections[connection].users) < 2:
+                return id
+        try:
+            return self._create_connection(user)
+        except TooManyUsersError:
+            return None
+        
+    def _get_connection_by_sender_id(self, sender_id: str) -> Connection:
+        for connection in self.connections.values():
+            for user in connection.users:
+                if user.username == sender_id:
+                    return connection
+        raise ValueError(f"User {sender_id} not found")
+
+    def Connect(self, request: ChatUser, context) -> ChatUserConnected:
+        user = request
+        id = self._get_or_create_available_connection(user)
+        status = id is not None
+        return ChatUserConnected(isConnected=status, conversationID=id)
+
+    def Disconnect(self, request: ChatUser, context) -> ChatUserConnected:
         status = self.connection.remove_user(request)
-        return chat.ChatUserConnected(isConnected=not status)
+        return ChatUserConnected(isConnected=not status)
 
-    def SendMessage(self, request: chat.Message, context) -> chat.Empty:
+    def SendMessage(self, request: Message, context) -> Empty:
         logging.info(f"User {request.senderID} has sent a message: {request.message}")
-        self.connection.add_to_chat(request)
-        return chat.Empty()
+        connection = _get_connection_by_sender_id(request.senderID)
+        tion.add_to_chat(request)
+        return Empty()
 
-    def SubscribeMessages(
-        self, request: chat.ChatUser, context
-    ):
+    def SubscribeMessages(self, request: ChatUser, context):
         client_id = request.username
         last_seen_message_index = 0
 
