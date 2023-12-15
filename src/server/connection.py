bda08a8ab2544ebc77ef67876861b7f2f0520ae7
ndoniyor <ndoniyor123@gmail.com>
2023-12-14 23:15:13 -0500

    Fix user addition logic in Connection class

--- src/server/connection.py
+++ src/server/connection.py
@@ -1,25 +1,26 @@
+from errors.errors import TooManyUsersError
 import server.proto.chat_pb2 as chat
 import server.proto.chat_pb2_grpc as rpc
 
 class Connection:
     def __init__(self):
-        self.users = ()
+        self.users = []
         self.chats = []
 
     @property
     def is_connected(self) -> bool:
         return len(self.users) == 2
 
-    def add_user(self, user: chat.ChatUser) -> bool:
+    def add_user(self, user: chat.ChatUser):
         if len(self.users) < 2:
-            self.users.add(user)
-            return True
+            self.users.append(user)
         else:
-            return False
+            raise TooManyUsersError
 
     def add_to_chat(self, chat):
         self.chats.append(chat)
 
+    # TODO: go over this
     def remove_user(self, user: chat.ChatUser) -> bool:
         for existing_user in self.users:
             if existing_user.username == user.username:
