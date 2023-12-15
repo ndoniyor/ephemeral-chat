30186e5629a767aebdb0121e967c38fb351b8986
ndoniyor <ndoniyor123@gmail.com>
2023-12-15 17:30:07 -0500

    Refactor MemoryConnectionManager to improve connection handling

--- src/connections/memory_manager.py
+++ src/connections/memory_manager.py
@@ -5,8 +5,6 @@ from errors.errors import ConversationNotFoundError, TooManyUsersError
 from server.connection import Connection
 from server.proto.chat_pb2 import ChatUser
 
-logging.basicConfig()
-
 
 class MemoryConnectionManager(ConnectionManager):
     # TODO: Directly add connection to available connections list
@@ -14,34 +12,43 @@ class MemoryConnectionManager(ConnectionManager):
         self.connections = {}
         self.available_connections = []
 
-    def _create_connection(self, user: ChatUser) -> str:
-        connection = Connection()
-        connection.add_user(user)
+    def _create_connection(self, user: ChatUser) -> Connection:
         id = str(uuid4())
-        self.available_connections.append(id)
-        self.connections[id] = connection
-        return id
+        new_connection = Connection(conversation_id=id)
+        new_connection.add_user(user)
 
-    def get_or_create_available_connection(self, user: ChatUser) -> str | None:
+        self.connections[id] = new_connection
+        self.available_connections.insert(0, new_connection)
+
+        return new_connection
+
+    def get_or_create_available_connection_id(self, user: ChatUser) -> str | None:
         if len(self.available_connections) > 0:
-            # TODO: Modify this so that conversations waiting the longest are prioritized
-            '''
-            So issue right now is I can either add conversations with O(1) with append, 
+            """
+            So issue right now is I can either add conversations with O(1) with append,
             and then remove with pop(0) which is O(n)
-            
+
             Or I can add conversations with O(n) with insert(0)
             and then remove with pop(-1) which is O(1)
-            
+
             Could also look into queues
-            '''
-            return self.available_connections.pop()
+            """
+            connection = self.available_connections[-1]
+            connection.add_user(user)
+            if connection.is_full:
+                self.connections[
+                    connection.conversation_id
+                ] = self.available_connections.pop()
+            return connection.conversation_id
         try:
-            return self._create_connection(user)
+            new_connection = self._create_connection(user)
+            return new_connection.conversation_id
+
         except TooManyUsersError:
             return None
 
-    def get_connection_by_client_id(
-        self, sender_id: str, conversation_id: str
+    def get_connection_by_conversation_id(
+        self, conversation_id: str
     ) -> Connection:
         logging.info(f"Looking for conversation: {conversation_id}")
         try:
