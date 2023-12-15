f0b364d789c639f574a7cfe55f87d85ec18fba1c
ndoniyor <ndoniyor123@gmail.com>
2023-12-15 17:30:19 -0500

    Fix user removal bug in Connection class

--- src/server/connection.py
+++ src/server/connection.py
@@ -2,17 +2,22 @@ from errors.errors import TooManyUsersError
 import server.proto.chat_pb2 as chat
 import server.proto.chat_pb2_grpc as rpc
 
+CHAT_LIMIT_DEFAULT = 3
+
+
 class Connection:
-    def __init__(self):
+    def __init__(self, conversation_id:str, user_limit: int = CHAT_LIMIT_DEFAULT):
         self.users = []
         self.chats = []
+        self.conversation_id = conversation_id
+        self.user_limit = user_limit
 
     @property
-    def is_connected(self) -> bool:
-        return len(self.users) == 2
+    def is_full(self) -> bool:
+        return len(self.users) >= self.user_limit
 
     def add_user(self, user: chat.ChatUser):
-        if len(self.users) < 2:
+        if len(self.users) < self.user_limit:
             self.users.append(user)
         else:
             raise TooManyUsersError
@@ -24,6 +29,6 @@ class Connection:
     def remove_user(self, user: chat.ChatUser) -> bool:
         for existing_user in self.users:
             if existing_user.username == user.username:
-                self.users.remove(user)
+                self.users.remove(existing_user)
                 return True
         return False
