cb194c339b7e81d365fe9750af3c1788cb417359
ndoniyor <ndoniyor123@gmail.com>
2023-12-15 00:45:02 -0500

    Refactor server.py: Add error handling and offload connection management to new class

--- src/server/server.py
+++ src/server/server.py
@@ -13,42 +13,24 @@ from server.proto.chat_pb2 import (
     Message,
 )
 from server.proto.chat_pb2_grpc import ChatServicer
-from errors.errors import TooManyUsersError
+from errors.errors import (
+    ConversationNotFoundError,
+    TooManyUsersError,
+    UserNotFoundError,
+)
 from server.connection import Connection
+from connections.memory_manager import MemoryConnectionManager
+
+logging.basicConfig()
 
 
 class Server(ChatServicer):
     def __init__(self):
         self.connections = {}
 
-    # TODO: Directly add connection to available connections list
-    def _create_connection(self, user: ChatUser) -> str:
-        connection = Connection()
-        connection.add_user(user)
-        id = str(uuid4())
-        self.connections[id] = connection
-        return id
-
-    # TODO: Maintain list of available connections to reduce time complexity of search
-    def _get_or_create_available_connection(self, user: ChatUser) -> str | None:
-        for id, connection in self.connections.items():
-            if len(self.connections[connection].users) < 2:
-                return id
-        try:
-            return self._create_connection(user)
-        except TooManyUsersError:
-            return None
-        
-    def _get_connection_by_sender_id(self, sender_id: str) -> Connection:
-        for connection in self.connections.values():
-            for user in connection.users:
-                if user.username == sender_id:
-                    return connection
-        raise ValueError(f"User {sender_id} not found")
-
     def Connect(self, request: ChatUser, context) -> ChatUserConnected:
         user = request
-        id = self._get_or_create_available_connection(user)
+        id = MemoryConnectionManager._get_or_create_available_connection(user)
         status = id is not None
         return ChatUserConnected(isConnected=status, conversationID=id)
 
@@ -58,18 +40,27 @@ class Server(ChatServicer):
 
     def SendMessage(self, request: Message, context) -> Empty:
         logging.info(f"User {request.senderID} has sent a message: {request.message}")
-        connection = _get_connection_by_sender_id(request.senderID)
-        tion.add_to_chat(request)
+        try:
+            connection = MemoryConnectionManager._get_connection_by_client_id(
+                request.senderID, request.conversationID
+            )
+            connection.add_to_chat(request)
+        except ConversationNotFoundError:
+            logging.error(f"Could not find conversation {request.conversationID}")
         return Empty()
 
     def SubscribeMessages(self, request: ChatUser, context):
         client_id = request.username
+        conversation_id = request.conversationID
         last_seen_message_index = 0
 
         logging.info(f"User {client_id} has subscribed to messages")
+        connection = MemoryConnectionManager._get_connection_by_client_id(
+            client_id, conversation_id
+        )
         while True:
-            while len(self.connection.chats) > last_seen_message_index:
-                message = self.connection.chats[last_seen_message_index]
+            while len(connection.chats) > last_seen_message_index:
+                message = connection.chats[last_seen_message_index]
                 last_seen_message_index += 1
                 if message.senderID != client_id:
                     yield message
