f9ce31651118622f52c0bca161dcd3ec49bbdd80
ndoniyor <ndoniyor123@gmail.com>
2023-12-15 17:30:01 -0500

    Refactor serve() function and add logging

--- src/serve.py
+++ src/serve.py
@@ -8,15 +8,19 @@ from server.server import Server
 ADDRESS = "localhost"
 PORT = 11912
 
+
 def serve():
+    logging.basicConfig(level=logging.INFO)
+
     server = grpc.server(ThreadPoolExecutor(max_workers=10))
     rpc.add_ChatServicer_to_server(Server(), server)
     server.add_insecure_port(f"[::]:{PORT}")
     server.start()
-    print("Listening")
+
+    logging.info("Listening")
+
     server.wait_for_termination()
 
 
 if __name__ == "__main__":
-    logging.basicConfig()
     serve()
