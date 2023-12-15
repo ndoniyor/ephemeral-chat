3955637e8605482a9eff1338bdf86957825bf401
ndoniyor <ndoniyor123@gmail.com>
2023-12-15 00:52:59 -0500

    Refactor connection management in MemoryConnectionManager

--- src/connections/memory_manager.py
+++ src/connections/memory_manager.py
@@ -1,6 +1,6 @@
 import logging
 from uuid import uuid4
-from connection_manager import ConnectionManager
+from connections.connection_manager import ConnectionManager
 from errors.errors import ConversationNotFoundError, TooManyUsersError
 from server.connection import Connection
 from server.proto.chat_pb2 import ChatUser
@@ -10,33 +10,33 @@ logging.basicConfig()
 
 class MemoryConnectionManager(ConnectionManager):
     # TODO: Directly add connection to available connections list
-    @classmethod
-    def _create_connection(cls, user: ChatUser) -> str:
+    def __init__(self):
+        self.connections = {}
+
+    def _create_connection(self, user: ChatUser) -> str:
         connection = Connection()
         connection.add_user(user)
         id = str(uuid4())
-        cls.connections[id] = connection
+        self.connections[id] = connection
         return id
 
     # TODO: Maintain list of available connections to reduce time complexity of search
-    @classmethod
-    def _get_or_create_available_connection(cls, user: ChatUser) -> str | None:
-        for id in cls.connections.keys():
-            connection = cls.connections[id]
+    def get_or_create_available_connection(self, user: ChatUser) -> str | None:
+        for id in self.connections.keys():
+            connection = self.connections[id]
             if len(connection.users) < 2:
                 connection.add_user(user)
                 return id
         try:
-            return cls._create_connection(user)
+            return self._create_connection(user)
         except TooManyUsersError:
             return None
 
-    @classmethod
-    def _get_connection_by_client_id(
-        cls, sender_id: str, conversation_id: str
+    def get_connection_by_client_id(
+        self, sender_id: str, conversation_id: str
     ) -> Connection:
         logging.info(f"Looking for conversation: {conversation_id}")
         try:
-            return cls.connections[conversation_id]
+            return self.connections[conversation_id]
         except KeyError:
             raise ConversationNotFoundError
