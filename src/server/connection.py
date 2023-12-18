d4ee2fec292807d2e938f1adba2851d41cd07ea9
ndoniyor <ndoniyor123@gmail.com>
2023-12-18 00:25:38 -0500

    simplify naming conventions

--- src/server/connection.py
+++ src/server/connection.py
@@ -2,19 +2,26 @@ from errors.errors import TooManyUsersError
 import server.proto.chat_pb2 as chat
 import server.proto.chat_pb2_grpc as rpc
 
-CHAT_LIMIT_DEFAULT = 3
+CHAT_LIMIT_DEFAULT = 2
 
 
 class Connection:
-    def __init__(self, conversation_id:str, user_limit: int = CHAT_LIMIT_DEFAULT):
+    def __init__(self, conversation_id: str, user_limit: int = CHAT_LIMIT_DEFAULT):
         self.users = []
         self.chats = []
         self.conversation_id = conversation_id
         self.user_limit = user_limit
+        self.is_active = False
 
     @property
     def is_full(self) -> bool:
         return len(self.users) >= self.user_limit
+
+    def activate(self):
+        self.is_active = True
+
+    def deactivate(self):
+        self.is_active = False
 
     def add_user(self, user: chat.ChatUser):
         if len(self.users) < self.user_limit:
@@ -26,7 +33,7 @@ class Connection:
         self.chats.append(chat)
 
     # TODO: go over this
-    def remove_user(self, user: chat.ChatUser) -> bool:
+    def disconnect_user(self, user: chat.ChatUser) -> bool:
         for existing_user in self.users:
             if existing_user.username == user.username:
                 self.users.remove(existing_user)
