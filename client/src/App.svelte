1cc53689b46fa9879717219b29263551b60589fe
ndoniyor <ndoniyor123@gmail.com>
2024-02-13 09:51:00 -0500

    moving react js to svelte ts

---
+++ client/src/App.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import { ChatUser, Empty, Message } from "./chat/chat.js";
+  import ChatClientServicer from './chat/chatClientServicer';
+
+  const chatClient = new ChatClientServicer();
+  let userName = '';
+
+  const handleClick = async () => {
+    chatClient.setUser(ChatUser.create({ username: userName }))
+    const response = await chatClient.connect();
+    
+    console.log(response);
+  };
+
+</script> 
+
+<main>
+  <div>
+    <input type="text" bind:value={userName} placeholder="Enter a username" />
+    <button on:click={handleClick}>Enter</button>
+  </div>
+</main>
+
+
+<style>
+
+</style>
