bb223c848a95d6b277f3a3852cc121b191a1a814
ndoniyor <ndoniyor123@gmail.com>
2024-02-22 14:08:54 -0500

    added daisyui

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	daisyui: {
+		themes: [
+			{
+				gruvbox: {
+					"primary": "#3c3836",
+					"secondary": "#fbf1c7",
+					"accent": "#fabd2f",
+					"neutral": "#282828",
+					"base-100": "#2b2b2b",
+					"info": "#83a5981 ",
+					"success": "#98971a",
+					"warning": "#d65d0e",
+					"error": "#cc241d",
+				},
+			},
+		],
+	},
+	plugins: [require("daisyui")],
+};
