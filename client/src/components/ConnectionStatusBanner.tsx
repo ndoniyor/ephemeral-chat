a1b52667290ce2266a0a631053e2dcdb4a214d3c
ndoniyor <ndoniyor123@gmail.com>
2024-03-07 11:26:30 -0500

    add component to handle connection status

---
+++ client/src/components/ConnectionStatusBanner.tsx
@@ -0,0 +1,21 @@
+function ConnectionStatusBanner({
+	connectionStatus, conversationID
+}: {
+	connectionStatus: boolean, conversationID: string
+}) {
+	return (
+		<div>
+			{connectionStatus ? (
+				<div className="card p-5 mb-5 bg-success">
+					<h2>You are connected to: {conversationID}</h2>
+				</div>
+			) : (
+				<div className="card p-5 mb-5 bg-warning">
+					<h2>You are not connected</h2>
+				</div>
+			)}
+		</div>
+	);
+}
+
+export default ConnectionStatusBanner;
