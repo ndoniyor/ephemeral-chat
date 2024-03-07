3b84969f99a4687e60c0d43e3ab15be349cd0b28
ndoniyor <ndoniyor123@gmail.com>
2024-03-07 11:26:46 -0500

    add message to handle connection

---
+++ client/src/components/UserCreationForm.tsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { useChatClient } from "../context/ChatClientContext";
+import { Message } from "../chat/protos/chat";
+
+function UserCreationForm({ setConnectionStatus, setConversationID, setMessages }: { setConnectionStatus: Function, setConversationID: Function, setMessages: Function}) {
+	
+	const [username, setUsername] = useState("");
+
+	const client = useChatClient()
+
+	const handleConnect = async () => {
+		console.log(client.id)
+		client.setUser(username);
+		const {status, conversationID, stream} = await client.connect()
+		if(stream){
+			stream.onMessage((message) => {
+				setMessages((prevMessages: Message[]) => [...prevMessages, message]);
+			});
+			setConnectionStatus(status);
+			setConversationID(conversationID);
+		}
+	};
+
+	return (
+		<div className="flex"> 
+			<input
+				id="username-field"
+				placeholder="Enter username"
+				className="input input-bordered w-full"
+				onChange={(e) => {
+					setUsername(e.target.value);
+				}}
+			/>
+			<button
+				className="btn btn-accent ml-5"
+				onClick={handleConnect}
+			>
+				Enter
+			</button>
+		</div>
+	);
+}
+
+export default UserCreationForm;
