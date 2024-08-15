1d37eeafd25354bae89ed8cbd2be91612bc4f9b4
ndoniyor <ndoniyor123@gmail.com>
2024-08-15 14:32:18 -0400

    navbar component

---
+++ client/src/components/NavBar.tsx
@@ -0,0 +1,12 @@
+function NavBar() {
+	return (
+		<div className="navbar bg-base-200 sticky top-0 w-screen z-50 pl-10">
+            {/* Logo */}
+			<div className="flex-1">
+				<p className="text-xl"><b>Ephemeral Chat</b></p>
+			</div>
+		</div>
+	);
+}
+
+export default NavBar;
